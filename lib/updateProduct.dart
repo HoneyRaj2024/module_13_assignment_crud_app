8edeb6c95c63d5f41ba9f33cbe370716af460302
pc <alsaadit2023@gmail.com>
2024-05-29 23:42:28 +0600

    Update

--- lib/updateProduct.dart
+++ lib/updateProduct.dart
@@ -8,7 +8,8 @@ import 'package:image_picker/image_picker.dart';
 class UpdateProductPage extends StatefulWidget {
   final int productId;
 
-  const UpdateProductPage({Key? key, required this.productId}) : super(key: key);
+  const UpdateProductPage({Key? key, required this.productId})
+      : super(key: key);
 
   @override
   _UpdateProductPageState createState() => _UpdateProductPageState();
@@ -42,7 +43,8 @@ class _UpdateProductPageState extends State<UpdateProductPage> {
 
   void _calculateTotalPrice() {
     setState(() {
-      final double unitPrice = double.tryParse(_unitPriceController.text) ?? 0.0;
+      final double unitPrice =
+          double.tryParse(_unitPriceController.text) ?? 0.0;
       final int quantity = int.tryParse(_quantityController.text) ?? 0;
       final double totalPrice = unitPrice * quantity;
       _totalPriceController.text = totalPrice.toStringAsFixed(2);
@@ -50,12 +52,11 @@ class _UpdateProductPageState extends State<UpdateProductPage> {
   }
 
   Future<void> _fetchProductData() async {
-    try {
-      final url = Uri.parse('https://crudapp.alsaaditsolution.com/rest-api/api/get_product.php?id=${widget.productId}');
-      final response = await http.get(url);
+    final url = Uri.parse(
+        'https://crudapp.alsaaditsolution.com/rest-api/api/get_product.php?id=${widget.productId}');
 
-      print("Response status: ${response.statusCode}");
-      print("Response body: ${response.body}");
+    try {
+      final response = await http.get(url);
 
       if (response.statusCode == 200) {
         final product = jsonDecode(response.body);
@@ -68,19 +69,19 @@ class _UpdateProductPageState extends State<UpdateProductPage> {
           _productCodeController.text = product['productcode'];
           _unitPriceController.text = unitPrice.toString();
           _quantityController.text = quantity.toString();
-          _totalPriceController.text = (unitPrice * quantity).toStringAsFixed(2);
+          _totalPriceController.text =
+              (unitPrice * quantity).toStringAsFixed(2);
           _imageUrl = product['img'];
           _isLoading = false;
         });
       } else {
-        _showErrorDialog('Failed to fetch product data. Please try again.');
+        throw Exception(
+            'Failed to fetch product data. StatusCode: ${response.statusCode}');
       }
     } catch (e) {
-      print("Error fetching product data: $e");
       _showErrorDialog('Failed to fetch product data. Please try again.');
     }
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -95,118 +96,122 @@ class _UpdateProductPageState extends State<UpdateProductPage> {
       body: _isLoading
           ? const Center(child: CircularProgressIndicator())
           : Container(
-        color: Colors.grey[200],
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  TextFormField(
-                    controller: _productNameController,
-                    decoration: const InputDecoration(labelText: 'Product Name'),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter product name';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 20.0),
-                  TextFormField(
-                    controller: _productCodeController,
-                    decoration: const InputDecoration(labelText: 'Product Code'),
-                    keyboardType: TextInputType.number,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter product code';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 20.0),
-                  TextFormField(
-                    controller: _unitPriceController,
-                    decoration: const InputDecoration(labelText: 'Unit Price'),
-                    keyboardType: TextInputType.number,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter unit price';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 20.0),
-                  TextFormField(
-                    controller: _quantityController,
-                    decoration: const InputDecoration(labelText: 'Quantity'),
-                    keyboardType: TextInputType.number,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter quantity';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 20.0),
-                  TextFormField(
-                    controller: _totalPriceController,
-                    decoration: const InputDecoration(labelText: 'Total Price'),
-                    keyboardType: TextInputType.number,
-                    enabled: false,
-                  ),
-                  const SizedBox(height: 20.0),
-                  Center(
-                    child: ElevatedButton(
-                      onPressed: _pickImage,
-                      child: const Text('Pick Image'),
+              color: Colors.grey[200],
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TextFormField(
+                          controller: _productNameController,
+                          decoration:
+                              const InputDecoration(labelText: 'Product Name'),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter product name';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(height: 20.0),
+                        TextFormField(
+                          controller: _productCodeController,
+                          decoration:
+                              const InputDecoration(labelText: 'Product Code'),
+                          keyboardType: TextInputType.number,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter product code';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(height: 20.0),
+                        TextFormField(
+                          controller: _unitPriceController,
+                          decoration:
+                              const InputDecoration(labelText: 'Unit Price'),
+                          keyboardType: TextInputType.number,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter unit price';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(height: 20.0),
+                        TextFormField(
+                          controller: _quantityController,
+                          decoration:
+                              const InputDecoration(labelText: 'Quantity'),
+                          keyboardType: TextInputType.number,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter quantity';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(height: 20.0),
+                        TextFormField(
+                          controller: _totalPriceController,
+                          decoration:
+                              const InputDecoration(labelText: 'Total Price'),
+                          keyboardType: TextInputType.number,
+                          enabled: false,
+                        ),
+                        const SizedBox(height: 20.0),
+                        if (_imageFile != null)
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20.0),
+                            child: Image.file(
+                              _imageFile!,
+                              height: 150,
+                            ),
+                          ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 20.0),
+                          child: _imageUrl != null
+                              ? Image.network(
+                                  'https://crudapp.alsaaditsolution.com/rest-api/api/img/$_imageUrl',
+                                  height: 150,
+                                )
+                              : Container(), // Placeholder or alternative widget if _imageUrl is null
+                        ),
+                        const SizedBox(height: 20.0),
+                        Center(
+                          child: ElevatedButton(
+                            onPressed: _pickImage,
+                            child: const Text('Pick Image'),
+                          ),
+                        ),
+                        const SizedBox(height: 20.0),
+                        Center(
+                          child: ElevatedButton(
+                            onPressed: _updateProduct,
+                            child: const Text('Update Product'),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  if (_imageFile != null)
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20.0),
-                      child: Image.file(
-                        _imageFile!,
-                        height: 150,
-                      ),
-                    )
-                  else if (_imageUrl != null)
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20.0),
-                      child: Image.network(
-                        _imageUrl!,
-                        height: 150,
-                      ),
-                    ),
-                  const SizedBox(height: 20.0),
-                  Center(
-                    child: ElevatedButton(
-                      onPressed: _updateProduct,
-                      child: const Text('Update Product'),
-                    ),
-                  ),
-                ],
+                ),
               ),
             ),
-          ),
-        ),
-      ),
     );
   }
 
   Future<void> _pickImage() async {
-    print("Pick image button clicked");
     try {
-      final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+      final pickedFile =
+          await ImagePicker().pickImage(source: ImageSource.gallery);
       if (pickedFile != null) {
         setState(() {
           _imageFile = File(pickedFile.path);
-          print("Image picked: ${_imageFile!.path}");
         });
-      } else {
-        print("User cancelled the image picker");
       }
     } catch (e) {
       print("Error picking image: $e");
@@ -216,11 +221,13 @@ class _UpdateProductPageState extends State<UpdateProductPage> {
   Future<void> _updateProduct() async {
     if (_formKey.currentState!.validate()) {
       try {
-        final url = Uri.parse('https://crudapp.alsaaditsolution.com/rest-api/api/update.php');
+        final url = Uri.parse(
+            'https://crudapp.alsaaditsolution.com/rest-api/api/update.php');
         final request = http.MultipartRequest('POST', url);
 
         if (_imageFile != null) {
-          request.files.add(await http.MultipartFile.fromPath('img', _imageFile!.path));
+          request.files
+              .add(await http.MultipartFile.fromPath('img', _imageFile!.path));
         }
 
         request.fields['productname'] = _productNameController.text;
@@ -229,12 +236,8 @@ class _UpdateProductPageState extends State<UpdateProductPage> {
         request.fields['quantity'] = _quantityController.text;
         request.fields['product_id'] = widget.productId.toString();
 
-        print("Sending request...");
         final response = await request.send();
-
         final result = await http.Response.fromStream(response);
-        print("Response received: ${result.statusCode}");
-        print("Response body: ${result.body}");
         if (response.statusCode == 200) {
           final responseData = jsonDecode(result.body);
           if (responseData['error'] != null) {
